c7cfe84e88391f4841ee4904cc156877655b4608
Alok Guha <aloksguha@gmail.com>
2014-08-20 10:35:32 +0530

    anges server.js

--- src/server.js
+++ src/server.js
@@ -48,19 +48,43 @@
       }, function(done) {
         return done(schema.validate(req.body));
       }, function(done) {
+        console.log("---------------------JSON Schema-------------------------");
+        console.log(req.body);
         return schema.genIpsums(req.body, req.query.n, done);
-      }
-    ], function(err, ipsums) {
+      }//,
+//      function(jsonObj,done){
+//          console.log("+++++++++++");
+//          console.log(jsonObj);
+//          console.log("++++++++++");
+//    	  return someExtraWays(jsonObj,done);
+//      }
+//
+   ]
+        , function(err, ipsums) {
       var response;
       if (err != null) {
         console.error(err);
         return res.send(400, err);
       } else {
+
         response = ipsums.length === 1 ? ipsums[0] : ipsums;
+          console.log("---------------------Generated JSON---------------------------");
+          console.log(JSON.stringify(response));
         return res.send(200, JSON.stringify(response, {}, "  "));
       }
     });
   });
+  
+  function someExtraWays(json, done){
+      console.log("coming here");
+      var resp = json.length === 1 ? json[0] : json;
+
+	  return done(resp);
+ 	  //console.log(json[0].name);
+	  
+	  
+  }
+  
   http.createServer(app).listen(app.get("port"), function() {
     return console.log("" + (app.get("name")) + " listening on port " + (app.get("port")));
   });
